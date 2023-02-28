323d3352886bdefecf9d23bc7f2809198042227e
pauromeu <romeu.pau@gmail.com>
2023-02-28 18:35:54 +0100

    change fP vector so it does not include dirichlets

--- code/serial/assembly_f_vectors.py
+++ code/serial/assembly_f_vectors.py
@@ -18,3 +18,10 @@ def assembly_fR_vector(mesh, fr_dat):
                                                        j*(mesh.Nsub_x*mesh.Nr) + i*(mesh.Nr_x) + mesh.Nsub_x*mesh.Nr_x*(sj - 1) + mesh.Nsub_x*(mesh.Nr_x - 2) + mesh.Nr_x)])
             fR[Rs.astype(int)] = fr_dat
     return fR
+
+
+def assembly_fP_vector(mesh, fP_dat):
+    indices = np.arange((mesh.Nsub_x + 1) * (mesh.Nsub_y + 1)) % (
+        mesh.Nsub_x + 1) != 0  # Remove Dirichlet nodes
+    fP = fP_dat[indices]
+    return fP
