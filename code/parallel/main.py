5c64bd954d7d7f6b9610f680c9a7c6504c5a4bd7
pauromeu <romeu.pau@gmail.com>
2023-05-15 16:46:35 +0200

    add main for parallel

---
+++ code/parallel/main.py
@@ -0,0 +1,11 @@
+# Import modules
+import numpy as np
+
+# Import functions
+from assembly_A_local_global_matrices import *
+from assembly_K_matrices import *
+from assembly_B_matrices import *
+from assembly_f_vectors import *
+from assembly_u_solution import *
+from RegularSudomainsMesh import RegularSubdomainsMesh
+from utils import *
