bbc9d9c0fbbb6d54e89c918202d3f2b8037a3645
pauromeu <romeu.pau@gmail.com>
2023-05-15 17:53:37 +0200

    adapt path

--- src/main_parallel.py
+++ src/main_parallel.py
@@ -1,5 +1,6 @@
 # Import modules
 import numpy as np
+import os
 
 # Import functions
 from common.assembly_A_local_global_matrices import *
@@ -10,11 +11,19 @@ from common.assembly_u_solution import *
 from common.RegularSudomainsMesh import RegularSubdomainsMesh
 from common.utils import *
 
-# Import data
-d_dat = np.genfromtxt('../data/small/d.dat')
-fP_dat = np.genfromtxt('../data/small/fP.dat')
-fr_dat = np.genfromtxt('../data/small/fr.dat')
-Ks = np.genfromtxt('../data/small/localK.dat')
-solution = np.genfromtxt('../data/small/solution.dat')
+# Get the path of the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-print('done!')
+# Construct the relative paths based on the current file's location
+d_path = os.path.join(current_dir, "..", "data", "small", "d.dat")
+fP_path = os.path.join(current_dir, "..", "data", "small", "fP.dat")
+fr_path = os.path.join(current_dir, "..", "data", "small", "fr.dat")
+Ks_path = os.path.join(current_dir, "..", "data", "small", "localK.dat")
+sol_path = os.path.join(current_dir, "..", "data", "small", "solution.dat")
+
+# Import the data using the relative paths
+d_dat = np.genfromtxt(d_path)
+fP_dat = np.genfromtxt(fP_path)
+fr_dat = np.genfromtxt(fr_path)
+Ks = np.genfromtxt(Ks_path)
+solution = np.genfromtxt(sol_path)
