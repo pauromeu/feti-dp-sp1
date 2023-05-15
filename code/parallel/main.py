3f3c9ecd36ed8272224b99978b68563028eb3d8c
pauromeu <romeu.pau@gmail.com>
2023-05-15 17:00:24 +0200

    add main imports

--- code/parallel/main.py
+++ code/parallel/main.py
@@ -9,3 +9,10 @@ from assembly_f_vectors import *
 from assembly_u_solution import *
 from RegularSudomainsMesh import RegularSubdomainsMesh
 from utils import *
+
+# Import data
+d_dat = np.genfromtxt('data/d.dat')
+fP_dat = np.genfromtxt('data/fP.dat')
+fr_dat = np.genfromtxt('data/fr.dat')
+Ks = np.genfromtxt('data/localK.dat')
+solution = np.genfromtxt('data/solution.dat')
