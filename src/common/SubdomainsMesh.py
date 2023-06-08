e271b0d8f8f36c6cd976eb4ba6396483643fd4c0
pauromeu <romeu.pau@gmail.com>
2023-06-08 19:30:40 +0200

    clean SubdomainsMesh

--- src/common/SubdomainsMesh.py
+++ src/common/SubdomainsMesh.py
@@ -1,8 +1,10 @@
 import numpy as np
+from mpi4py import MPI
+
 from common.assembly_A_local_global_matrices import *
 from common.assembly_K_matrices import *
-from mpi4py import MPI
 from common.mesh_solvers import solve_mesh, solve_parallel_mesh
+from common.mesh_utils import get_F_condition_number_mesh, get_remaining_numeration_mesh, plot_u_boundaries_mesh
 from parallel.cg_feti import *
 from common.utils import *
 
@@ -238,8 +240,8 @@ class SubdomainsMesh:
 
     def set_d_vector(self):
         # TODO adapt for Dirichlet conditions distinct to 0
-        self.d = np.zeros(self.NLambda)
         # self.d[self.NlambdaR:] = self[np.arange(len(d_dat)) % (self.Nr_y - 1) != 0]
+        self.d = np.zeros(self.NLambda)
 
     def set_f_vectors(self):
         self.fR = assembly_fR_vector(self)
@@ -251,36 +253,11 @@ class SubdomainsMesh:
         self.uD = np.zeros(self.ND)
 
     def get_remaining_numeration(self, rs_array):
-        rs_array_renum = np.array([])
-        qs = np.sort(self.qs)[::-1]
-        assert np.all(np.intersect1d(qs, rs_array).size ==
-                      0), "Some remaining nodes have primal numeration"
-        assert np.all(rs_array < self.Nr + len(qs)
-                      ), "Numeration must be within the number of maximum nodes"
-        for r in rs_array:
-            assigned = False
-            for i, q in enumerate(qs):
-                if r > q:
-                    r_renum = r - (len(qs) - i)
-                    assigned = True
-                    break
-            if assigned == False:
-                r_renum = r
-            rs_array_renum = np.append(rs_array_renum, r_renum)
-        return rs_array_renum.astype(int)
-
-    def assembly_u_solution_big(self):
-        N = self.Ntot
-        u = np.array()
+        return get_remaining_numeration_mesh(self, rs_array)
 
     def get_F_condition_number(self):
-        KRR_inv = np.linalg.inv(self.KRR)
-        self.SPP = self.KPP - self.KPR @ KRR_inv @ self.KRP
-        SPP_inv = np.linalg.inv(self.SPP)
-        IR = np.eye(self.NR)  # RxR identity matrix
-        self.F = -self.BlambdaR @ KRR_inv @ (self.KRP @ SPP_inv @
-                                             self.KPR @ KRR_inv + IR) @ self.BlambdaR.T
-        self.cond_num = np.linalg.cond(self.F)
+        self.F, self.SPP, self.cond_num = get_F_condition_number_mesh(self)
+        return self.cond_num
 
     def solve(self):
         self.uP, self.uR = solve_mesh(self)
@@ -310,101 +287,8 @@ class SubdomainsMesh:
         self.set_uD_vector()
         self.get_F_condition_number()
         run_info = self.solve_parallel(comm, size, rank, True)
-        if returns_run_info:    
+        if returns_run_info:
             return run_info
-        
 
     def plot_u_boundaries(self):
-        Nbound_x = (self.Nsub_x + 1) + self.Nsub_x * len(self.bottom_r)
-        Nbound_y = (self.Nsub_y + 1) + self.Nsub_y * len(self.left_r)
-        Nbound_xs = len(self.bottom_r) + 2
-        Nbound_ys = len(self.left_r) + 2
-
-        u = np.zeros(Nbound_x * Nbound_y) - np.max(self.uR) * 0.15
-
-        idxs_D_uD = np.arange(
-            0,
-            Nbound_x*(Nbound_ys - 1)*self.Nsub_y + 1,
-            Nbound_x*(Nbound_ys - 1)
-        )
-
-        u[idxs_D_uD] = self.uD
-
-        idxs_P_uD = np.array([])
-        idxs_bot_uD = np.array([])
-        for j in range(self.Nsub_y + 1):
-            idxs_P_uD_j = np.arange(
-                Nbound_x*(Nbound_ys - 1)*j,
-                Nbound_x*(Nbound_ys - 1)*j +
-                (Nbound_xs - 1)*self.Nsub_x + 1,
-                (Nbound_xs - 1)
-            )
-
-            idxs_P_uD = np.concatenate((
-                idxs_P_uD,
-                idxs_P_uD_j
-            ))
-
-            idxs_bot_uD_j = np.arange(
-                Nbound_x*(Nbound_ys - 1)*j,
-                Nbound_x*(Nbound_ys - 1)*j + (Nbound_xs - 1)*self.Nsub_x + 1
-            )
-
-            idxs_bot_uD_j = np.setdiff1d(idxs_bot_uD_j, idxs_P_uD)
-            idxs_bot_uD = np.concatenate((idxs_bot_uD, idxs_bot_uD_j))
-
-        idxs_P_uD = idxs_P_uD.astype(int)
-        idxs_P_uD = np.setdiff1d(idxs_P_uD, idxs_D_uD.astype(int))
-        idxs_bot_uD = idxs_bot_uD.astype(int)
-
-        bottom_bound_r = self.get_remaining_numeration(self.bottom_r)
-        top_bound_r = self.get_remaining_numeration(self.top_r)
-
-        idxs_bot_uR = np.array([])
-        for s in range(self.Nsub_x*self.Nsub_y):
-            idxs_bot_uR = np.concatenate(
-                (idxs_bot_uR, bottom_bound_r + s*self.Nr)).astype(int)
-
-        for i in range(self.Nsub_x):
-            offset = (self.Nsub_x*(self.Nsub_y - 1)) * self.Nr
-            idxs_bot_uR = np.concatenate((
-                idxs_bot_uR, top_bound_r + i*self.Nr + offset)).astype(int)
-
-        idxs_left_uD = []
-        for i in range(self.Nsub_x + 1):
-            idxs_left_uD_j = np.arange(
-                i*(Nbound_xs - 1) + 0,
-                i*(Nbound_xs - 1) + Nbound_x*Nbound_y,
-                Nbound_x
-            )
-            idxs_left_uD_j = np.setdiff1d(idxs_left_uD_j, idxs_P_uD)
-            idxs_left_uD_j = np.setdiff1d(idxs_left_uD_j, idxs_D_uD)
-            idxs_left_uD = np.concatenate((idxs_left_uD, idxs_left_uD_j))
-
-        idxs_left_uD = idxs_left_uD.astype(int)
-
-        left_bound_r = self.get_remaining_numeration(self.left_r)
-        right_bound_r = self.get_remaining_numeration(self.right_r)
-
-        idxs_left_uR = np.array([])
-        for i in range(self.Nsub_x):
-            for j in range(self.Nsub_y):
-                offset = (j*self.Nsub_x + i) * self.Nr
-                idxs_left_uR = np.concatenate(
-                    (idxs_left_uR, left_bound_r + offset)
-                )
-
-        for j in range(self.Nsub_y):
-            offset = (self.Nsub_x*j + self.Nsub_x - 1)*self.Nr
-            idxs_left_uR = np.concatenate(
-                (idxs_left_uR, right_bound_r + offset)
-            )
-
-        idxs_left_uR = idxs_left_uR.astype(int)
-
-        u[idxs_P_uD] = self.uP
-        u[idxs_bot_uD] = self.uR[idxs_bot_uR]
-        u[idxs_left_uD] = self.uR[idxs_left_uR]
-
-        u_mat = u.reshape((Nbound_y, Nbound_x))
-        plot_sparse_matrix(u_mat, 'Solution - u field')
+        plot_u_boundaries_mesh(self)
---
+++ src/common/mesh_utils.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+from common.utils import plot_sparse_matrix
+
+
+def plot_u_boundaries_mesh(mesh):
+    Nbound_x = (mesh.Nsub_x + 1) + mesh.Nsub_x * len(mesh.bottom_r)
+    Nbound_y = (mesh.Nsub_y + 1) + mesh.Nsub_y * len(mesh.left_r)
+    Nbound_xs = len(mesh.bottom_r) + 2
+    Nbound_ys = len(mesh.left_r) + 2
+
+    u = np.zeros(Nbound_x * Nbound_y) - np.max(mesh.uR) * 0.15
+
+    idxs_D_uD = np.arange(
+        0,
+        Nbound_x*(Nbound_ys - 1)*mesh.Nsub_y + 1,
+        Nbound_x*(Nbound_ys - 1)
+    )
+
+    u[idxs_D_uD] = mesh.uD
+
+    idxs_P_uD = np.array([])
+    idxs_bot_uD = np.array([])
+    for j in range(mesh.Nsub_y + 1):
+        idxs_P_uD_j = np.arange(
+            Nbound_x*(Nbound_ys - 1)*j,
+            Nbound_x*(Nbound_ys - 1)*j +
+            (Nbound_xs - 1)*mesh.Nsub_x + 1,
+            (Nbound_xs - 1)
+        )
+
+        idxs_P_uD = np.concatenate((
+            idxs_P_uD,
+            idxs_P_uD_j
+        ))
+
+        idxs_bot_uD_j = np.arange(
+            Nbound_x*(Nbound_ys - 1)*j,
+            Nbound_x*(Nbound_ys - 1)*j + (Nbound_xs - 1)*mesh.Nsub_x + 1
+        )
+
+        idxs_bot_uD_j = np.setdiff1d(idxs_bot_uD_j, idxs_P_uD)
+        idxs_bot_uD = np.concatenate((idxs_bot_uD, idxs_bot_uD_j))
+
+    idxs_P_uD = idxs_P_uD.astype(int)
+    idxs_P_uD = np.setdiff1d(idxs_P_uD, idxs_D_uD.astype(int))
+    idxs_bot_uD = idxs_bot_uD.astype(int)
+
+    bottom_bound_r = mesh.get_remaining_numeration(mesh.bottom_r)
+    top_bound_r = mesh.get_remaining_numeration(mesh.top_r)
+
+    idxs_bot_uR = np.array([])
+    for s in range(mesh.Nsub_x*mesh.Nsub_y):
+        idxs_bot_uR = np.concatenate(
+            (idxs_bot_uR, bottom_bound_r + s*mesh.Nr)).astype(int)
+
+    for i in range(mesh.Nsub_x):
+        offset = (mesh.Nsub_x*(mesh.Nsub_y - 1)) * mesh.Nr
+        idxs_bot_uR = np.concatenate((
+            idxs_bot_uR, top_bound_r + i*mesh.Nr + offset)).astype(int)
+
+    idxs_left_uD = []
+    for i in range(mesh.Nsub_x + 1):
+        idxs_left_uD_j = np.arange(
+            i*(Nbound_xs - 1) + 0,
+            i*(Nbound_xs - 1) + Nbound_x*Nbound_y,
+            Nbound_x
+        )
+        idxs_left_uD_j = np.setdiff1d(idxs_left_uD_j, idxs_P_uD)
+        idxs_left_uD_j = np.setdiff1d(idxs_left_uD_j, idxs_D_uD)
+        idxs_left_uD = np.concatenate((idxs_left_uD, idxs_left_uD_j))
+
+    idxs_left_uD = idxs_left_uD.astype(int)
+
+    left_bound_r = mesh.get_remaining_numeration(mesh.left_r)
+    right_bound_r = mesh.get_remaining_numeration(mesh.right_r)
+
+    idxs_left_uR = np.array([])
+    for i in range(mesh.Nsub_x):
+        for j in range(mesh.Nsub_y):
+            offset = (j*mesh.Nsub_x + i) * mesh.Nr
+            idxs_left_uR = np.concatenate(
+                (idxs_left_uR, left_bound_r + offset)
+            )
+
+    for j in range(mesh.Nsub_y):
+        offset = (mesh.Nsub_x*j + mesh.Nsub_x - 1)*mesh.Nr
+        idxs_left_uR = np.concatenate(
+            (idxs_left_uR, right_bound_r + offset)
+        )
+
+    idxs_left_uR = idxs_left_uR.astype(int)
+
+    u[idxs_P_uD] = mesh.uP
+    u[idxs_bot_uD] = mesh.uR[idxs_bot_uR]
+    u[idxs_left_uD] = mesh.uR[idxs_left_uR]
+
+    u_mat = u.reshape((Nbound_y, Nbound_x))
+    plot_sparse_matrix(u_mat, 'Solution - u field')
+
+
+def get_remaining_numeration_mesh(mesh, rs_array):
+    rs_array_renum = np.array([])
+    qs = np.sort(mesh.qs)[::-1]
+    assert np.all(np.intersect1d(qs, rs_array).size ==
+                  0), "Some remaining nodes have primal numeration"
+    assert np.all(rs_array < mesh.Nr + len(qs)
+                  ), "Numeration must be within the number of maximum nodes"
+    for r in rs_array:
+        assigned = False
+        for i, q in enumerate(qs):
+            if r > q:
+                r_renum = r - (len(qs) - i)
+                assigned = True
+                break
+        if assigned == False:
+            r_renum = r
+        rs_array_renum = np.append(rs_array_renum, r_renum)
+    return rs_array_renum.astype(int)
+
+
+def get_F_condition_number_mesh(mesh):
+    KRR_inv = np.linalg.inv(mesh.KRR)
+    SPP = mesh.KPP - mesh.KPR @ KRR_inv @ mesh.KRP
+    SPP_inv = np.linalg.inv(SPP)
+    IR = np.eye(mesh.NR)  # RxR identity matrix
+    F = -mesh.BlambdaR @ KRR_inv @ (mesh.KRP @ SPP_inv @
+                                    mesh.KPR @ KRR_inv + IR) @ mesh.BlambdaR.T
+    cond_num = np.linalg.cond(mesh.F)
+    return [F, SPP, cond_num]
